dcc55f313ab371b3bd250dc0219a0728079122d3
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-20 03:45:01 -0400

    Rename arguments in `rsqrt`

--- src/FastInverseSqrt.jl
+++ src/FastInverseSqrt.jl
@@ -3,24 +3,22 @@ module FastInverseSqrt
 export rsqrt
 
 # See https://en.wikipedia.org/wiki/Fast_inverse_square_root#Overview_of_the_code & https://www.youtube.com/watch?v=p8u_k2LIZyo
-function rsqrt(number::Float32)
-    x2 = number * 0.5f0
-    y = number
-    i = reinterpret(UInt32, y)
-    i = 0x5f3759df - i >> 1
-    y = reinterpret(Float32, i)
-    y *= 1.5f0 - x2 * y^2
-    return y
+function rsqrt(x::Float32)
+    xₛ = x
+    int32 = reinterpret(UInt32, xₛ)
+    int32 = 0x5f3759df - int32 >> 1
+    xₛ = reinterpret(Float32, int32)
+    xₛ *= 1.5f0 - x * 0.5f0 * xₛ^2
+    return xₛ
 end
-function rsqrt(number::Float64)
-    x2 = number * 0.5
-    y = number
-    i = reinterpret(UInt64, y)
-    i = 0x5fe6eb50c7b537a9 - i >> 1  # See https://stackoverflow.com/a/11644533/3260253
-    y = reinterpret(Float64, i)
-    y *= 1.5 - x2 * y^2
-    return y
+function rsqrt(x::Float64)
+    xₛ = x
+    int64 = reinterpret(UInt64, xₛ)
+    int64 = 0x5fe6eb50c7b537a9 - int64 >> 1  # See https://stackoverflow.com/a/11644533/3260253
+    xₛ = reinterpret(Float64, int64)
+    xₛ *= 1.5 - x * 0.5 * xₛ^2
+    return xₛ
 end
-rsqrt(number::Real) = rsqrt(float(number))
+rsqrt(x::Real) = rsqrt(float(x))
 
 end
