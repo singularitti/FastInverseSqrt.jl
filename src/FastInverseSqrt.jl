193682a529a35584f2dd82a52e9eadf29cbb1d30
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-20 03:13:35 -0400

    Add `rsqrt`

--- src/FastInverseSqrt.jl
+++ src/FastInverseSqrt.jl
@@ -1,5 +1,25 @@
 module FastInverseSqrt
 
-# Write your package code here.
+export rsqrt
+
+# See https://en.wikipedia.org/wiki/Fast_inverse_square_root#Overview_of_the_code & https://www.youtube.com/watch?v=p8u_k2LIZyo
+function rsqrt(number::Float32)
+    x2 = number * 0.5f0
+    y = number
+    i = reinterpret(UInt32, y)
+    i = 0x5f3759df - i >> 1
+    y = reinterpret(Float32, i)
+    y *= 1.5f0 - x2 * y^2
+    return y
+end
+function rsqrt(number::Float64)
+    x2 = number * 0.5
+    y = number
+    i = reinterpret(UInt64, y)
+    i = 0x5fe6eb50c7b537a9 - i >> 1  # See https://stackoverflow.com/a/11644533/3260253
+    y = reinterpret(Float64, i)
+    y *= 1.5 - x2 * y^2
+    return y
+end
 
 end
