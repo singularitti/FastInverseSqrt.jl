0e3f3eb6f3bdbee22ccb39c7ee74d56337385d33
Documenter.jl <documenter@juliadocs.github.io>
2021-10-20 06:40:37 +0000

    build based on c1ff32a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastInverseSqrt","category":"page"},{"location":"#FastInverseSqrt","page":"Home","title":"FastInverseSqrt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastInverseSqrt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastInverseSqrt]","category":"page"}]
+}
